685582b319c5098eba3814fe09f518854693ff8a
osoleimani75 <osoleimani75@gmail.com>
2019-04-02 00:26:06 -0700

    Add files via upload

---
+++ app/data/friendsData.js
@@ -0,0 +1,56 @@
+const friendsArray =[
+  {
+      name:"Ahmed",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBUCSAlDoshtKFvWO982lbWoyeJaXVaBWPT3twNaIaeXIfGgqD",
+      scores:[
+        "5",
+        "5",
+        "5",
+        "5",
+        "4",
+        "4",
+        "5",
+        "4",
+        "5",
+        "4"
+        ]
+    },
+    {
+      name:"ali",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBUCSAlDoshtKFvWO982lbWoyeJaXVaBWPT3twNaIaeXIfGgqD",
+      scores:[
+        "1",
+        "3",
+        "5",
+        "5",
+        "3",
+        "4",
+        "5",
+        "2",
+        "5",
+        "4"
+        ]
+    },
+    {
+      name:"omid",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBUCSAlDoshtKFvWO982lbWoyeJaXVaBWPT3twNaIaeXIfGgqD",
+      scores:[
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        ]
+    }      
+
+
+
+
+  ]
+
+module.exports = friendsArray;
